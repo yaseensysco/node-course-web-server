81b48ab745277819c0e578c094d8e69df5e0fd99
Mohamed  Yaseen <Mohamed.Yaseen@trycake.com>
2017-08-16 16:00:25 +0530

    add project page

--- server.js
+++ server.js
@@ -37,6 +37,11 @@ app.get('/about',(req,res)=>{
     pageTitle:'About Page'
   });
 });
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  });
+});
 app.get('/bad',(req,res)=>{
   res.send({
     errorMessage:'Unable to handle the result.'
